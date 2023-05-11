1d5d258c6c811e99dc430c9441a69692e242139e
dv <dvpublic0@gmail.com>
2023-05-11 18:43:10 +0700

    interface example

---
+++ contracts/interfaces.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IInterfaceExample1 {
+  function uintField() external view returns (uint);
+  function mappingField(uint index) external view returns (address);
+  function ordinalFunction() external view returns (uint);
+}
+
+interface IInterfaceExample2 {
+  function addressField() external view returns (address);
+  function mappingField(uint index) external view returns (address);
+}
+
+contract HeirExample is IInterfaceExample1, IInterfaceExample2 {
+  uint public override uintField;
+  address public override addressField;
+  mapping(uint => address) public override(IInterfaceExample1, IInterfaceExample2) mappingField;
+  function ordinalFunction() external view external returns (uint) {
+    return 0;
+  }
+}
