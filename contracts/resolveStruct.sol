80d6fc7fc76401240de17a6a7e84752b92212ff5
dv <dvpublic0@gmail.com>
2023-07-13 19:54:02 +0700

    add resolve-struct example

---
+++ contracts/resolveStruct.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface A {
+  struct S {
+    uint a;
+  }
+}
+
+interface B {
+  struct S {
+    uint b;
+  }
+}
+
+struct S {
+  uint s;
+}
+
+contract C is B {
+  function que() view internal {
+    A.S memory shouldBeA;
+    S memory whichOne;
+    whichOne.
+  }
+}
