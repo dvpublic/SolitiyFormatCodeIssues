2d628cf39fc14a977a768a9a025e2c9df9320b51
dv <dvpublic0@gmail.com>
2024-03-01 14:29:19 +0700

    payable cases..

--- contracts/payableExamples.sol
+++ contracts/payableExamples.sol
@@ -9,15 +9,16 @@ contract B is A {
   function mint() external payable { }
   receive() external payable { }
 }
-
+contract C is A {
+  function mint() external payable { }
+}
 contract PayableExamples {
   function useAB(address addressB, B b, A a) external {
-
     // case 1: cast not-payable => A
     A(addressB).mint();
 
     // case 2: cast not-payable => B
-    // B(addressB).mint(); // not compiled
+    // B(addressB).mint(); // Explicit type conversion not allowed from non-payable "address" to "contract B", which has a payable fallback function.
 
     // case 3: cast B => A
     A(b).mint();
@@ -30,5 +31,25 @@ contract PayableExamples {
 
     // case 6: cast address => payable => A
     A(payable(addressB)).mint();
+  }
+
+  function useAC(address addressC, C c, A a) external {
+    // case 1: cast not-payable => A
+    A(addressC).mint();
+
+    // case 2: cast not-payable => B
+    // B(addressB).mint(); // not compiled
+
+    // case 3: cast B => A
+    A(c).mint();
+
+    // case 4: cast payable => B, not compiled
+    // C(payable(c)).mint(); // Explicit type conversion not allowed from "contract C" to "address payable".
+
+    // case 5: cast payable => A
+    // A(payable(c)).mint(); //  Explicit type conversion not allowed from "contract C" to "address payable".
+
+    // case 6: cast address => payable => A
+    A(payable(addressC)).mint();
   }
 }
